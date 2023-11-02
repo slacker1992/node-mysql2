2ba5ce5d94a50b948a29fa4afd8108619bf37939
slacker1992 <suvinjavax@gmail.com>
2023-11-02 18:49:41 +0530

    added mysql

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2";
 
 const app = express();
 app.use(express.json());
 
+const connection = mysql
+  .createConnection({
+    host: "178.16.143.25",
+    user: "sama",
+    password: "Samanthi1993@",
+    database: "royce_world",
+  })
+  .promise();
+
+await connection
+  .query(`select * from Types`)
+  .then(([rows]) => {
+    console.log(rows);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(4000, () => {
   console.log("server is running on port 4000");
 });
