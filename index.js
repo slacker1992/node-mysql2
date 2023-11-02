80b34de876fb6196298563512485750df214c015
slacker1992 <suvinjavax@gmail.com>
2023-11-02 19:16:00 +0530

    added mysql

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
-
+dotenv.config();
 const connection = mysql
   .createConnection({
     host: "localhost",
@@ -22,6 +23,10 @@ await connection
   .catch((err) => {
     console.log(err);
   });
-app.listen(4000, () => {
-  console.log("server is running on port 4000");
+
+app.use("/", (req, res) => {
+  res.send("working");
+});
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
 });
